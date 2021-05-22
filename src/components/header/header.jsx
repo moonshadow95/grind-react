a33c60d78eaaca17be4562261c03d7f1ef3cd481
Moonshadow <last_enemy@naver.com>
2021-05-22 13:56:21 +0900

    Fix routing and button style

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,35 +1,46 @@
 import React from 'react';
+import { useRouteMatch } from 'react-router';
+import { Link } from 'react-router-dom';
 import styles from './header.module.css';
 
-const Header = ({ onLogout }) => (
-  <div>
-    <header className={`${styles.header} ${onLogout && styles.fixed}`}>
-      <h1 className={styles.title}>Grind.</h1>
-      {onLogout && (
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            <li className={styles.item}>
-              <button className={styles.button}>Front</button>
-            </li>
-            <li className={styles.item}>
-              <button className={styles.button}>Back</button>
-            </li>
-            <li className={styles.item}>
-              <button className={styles.button}>Leg</button>
-            </li>
-            <li className={styles.item}>
-              <button className={styles.button}>Arm</button>
-            </li>
-            <li className={styles.item}>
-              <button className={styles.logout} onClick={onLogout}>
-                Logout
-              </button>
-            </li>
-          </ul>
-        </nav>
-      )}
-      <div className={styles.line}></div>
-    </header>
-  </div>
-);
+const Header = ({ onLogout }) => {
+  let { url } = useRouteMatch();
+  return (
+    <div>
+      <header className={`${styles.header} ${onLogout && styles.fixed}`}>
+        {onLogout ? (
+          <Link to="/home">
+            <h1 className={styles.title}>Grind.</h1>
+          </Link>
+        ) : (
+          <h1 className={styles.title}>Grind.</h1>
+        )}
+        {onLogout && (
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li className={styles.item}>
+                <button className={styles.button}>Front</button>
+              </li>
+              <li className={styles.item}>
+                <button className={styles.button}>Back</button>
+              </li>
+              <li className={styles.item}>
+                <button className={styles.button}>Leg</button>
+              </li>
+              <li className={styles.item}>
+                <button className={styles.button}>Arm</button>
+              </li>
+              <li className={styles.item}>
+                <button className={styles.logout} onClick={onLogout}>
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </nav>
+        )}
+        <div className={styles.line}></div>
+      </header>
+    </div>
+  );
+};
 export default Header;
--- src/components/records/records.jsx
+++ src/components/records/records.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
 import Record from '../record/record';
 import styles from './records.module.css';
 
 const Records = ({ records }) => {
+  let { path, url } = useRouteMatch();
   const [isToggleOn, setIsToggleOn] = useState(true);
   const onToggle = () => {
     if (!isToggleOn) {
@@ -16,13 +18,24 @@ const Records = ({ records }) => {
       {Object.keys(records).map((key) => (
         <Record key={key} record={records[key]} toggle={isToggleOn} />
       ))}
-      <button
-        className={`${styles.button} ${isToggleOn ? styles.show : styles.hide}`}
-        onClick={onToggle}
-      >
-        Summary
-        <i className="fas fa-sort-down"></i>
-      </button>
+      <div className={styles.container}>
+        <button
+          className={`${styles.buttonSum} ${
+            isToggleOn ? styles.show : styles.hide
+          }`}
+          onClick={onToggle}
+        >
+          <span>Summary</span>
+          <i className="fas fa-sort-down"></i>
+        </button>
+
+        <Link to="../diet">
+          <button className={styles.buttonDiet}>
+            <span>Diet</span>
+            <i className="fas fa-exchange-alt"></i>
+          </button>
+        </Link>
+      </div>
     </section>
   );
 };
